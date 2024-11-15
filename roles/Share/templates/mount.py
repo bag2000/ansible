da149949a4759c8e8647cb4dc6ce7cad5155c5fc
Polyakov Roman <polyakov.r.v@yandex.ru>
2024-11-15 15:42:11 +0300

    modified:   roles/Share/run.yml modified:   roles/Share/templates/mount.py

--- roles/Share/templates/mount.py
+++ roles/Share/templates/mount.py
@@ -8,7 +8,7 @@ while True:
     out = stdout.decode("utf-8").strip()
 
     if out == '0':
-        cmd_2 = '/usr/bin/kinit -k -t /etc/krb5-2.keytab test-user@T8.CORP'
+        cmd_2 = '/usr/bin/kinit -k -t /etc/krb5-2.keytab {{ kytab_user }}'
         cmd_3 = 'mount -t cifs -o sec=krb5,multiuser,file_mode=0600,dir_mode=0700 //{{ srv_name_lower }}{{ share_name_modded }} /mnt/SharedDocs/{{ dst_share_name }}'
 
         ps_2 = subprocess.Popen(cmd_2, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
